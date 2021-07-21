bbbab4cf1955aed2bf6d14cb36326486dba8c1fd
Miwalessa <85528421+Miwalessa@users.noreply.github.com>
2021-07-20 22:04:01 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pictureapp/ImageInCard.dart';
+import 'package:pictureapp/OnlyImage.dart';
+
+
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    //home: MeuApp(),
+    home: OnlyImage(),
+  ));
+}
